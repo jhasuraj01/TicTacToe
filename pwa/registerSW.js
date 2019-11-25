d897a2e8fd62554ec0f3af807ce5f12fc66caa59
the-suraj <surajsjjha@gmail.com>
2019-11-26 01:48:53 +0530

    changed manifest location

--- pwa/registerSW.js
+++ pwa/registerSW.js
@@ -4,7 +4,14 @@ let registerSW = () => {
         navigator.serviceWorker
             .register('../serviceworker2.js')
             .then(reg => console.log('Service Worker Registered'))
-            .catch(err => console.log('Service Worker Registeration error: '+ err));
+            .catch(err => console.log('Service Worker Registeration error: ' + err));
     }
 }
 window.addEventListener('load', registerSW);
+window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('APP installation prompt will be called')
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    showInstallPromotion();
+});
