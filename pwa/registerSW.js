d67d0cc48ba9fc9a5e44100d1f713ea82792b462
the-suraj <surajsjjha@gmail.com>
2019-11-26 02:21:56 +0530

    changed manifest location and changed theme color

--- pwa/registerSW.js
+++ pwa/registerSW.js
@@ -8,10 +8,3 @@ let registerSW = () => {
     }
 }
 window.addEventListener('load', registerSW);
-window.addEventListener('beforeinstallprompt', (e) => {
-    console.log('APP installation prompt will be called')
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    showInstallPromotion();
-});
