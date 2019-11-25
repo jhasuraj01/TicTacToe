09cda4391ffe44d1b3a9a50561a6cc214ef6de3d
the-suraj <surajsjjha@gmail.com>
2019-11-26 00:59:05 +0530

    Converted to PWA

---
+++ pwa/registerSW.js
@@ -0,0 +1,10 @@
+let registerSW = () => {
+    // first check if service worker is supported in the browser
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('../serviceworker2.js')
+            .then(reg => console.log('Service Worker Registered'))
+            .catch(err => console.log('Service Worker Registeration error: '+ err));
+    }
+}
+window.addEventListener('load', registerSW);
