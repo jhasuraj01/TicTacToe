c31bfa096b81e30187769932d8e5ba583a72cc1b
the-suraj <surajsjjha@gmail.com>
2019-11-26 01:59:23 +0530

    written scope in register function

--- pwa/registerSW.js
+++ pwa/registerSW.js
@@ -2,7 +2,7 @@ let registerSW = () => {
     // first check if service worker is supported in the browser
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('../serviceworker2.js')
+            .register('../serviceworker2.js', { scope: '/TicTacToe/' })
             .then(reg => console.log('Service Worker Registered'))
             .catch(err => console.log('Service Worker Registeration error: ' + err));
     }
